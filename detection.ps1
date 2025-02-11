12b093f87ad7558bbd5009ffb6de476e0267be14
slaptat <uzernameic@gmail.com>
2025-02-10 19:31:53 -0600

    Rev2 Detect

--- detection.ps1
+++ detection.ps1
@@ -1,11 +1,12 @@
 <#
 IFT380 Mod4  - NOCorps Lab 2 
-Detection
-#>
+Detection Rev-2
+#> 
 
 $prived =  icacls $env:windir\system32\config\sam 
+$listem = vssadmin list shadows | Out-String
 
-if ($prived -like "*Everyone|BUILTIN\Users*") {
+if ($prived -like "*Everyone|BUILTIN\Users*" -or $listem -like "*Contents of shadow copy*") {
     # We have access, system is vulnerable...probably actually a failure
     return "Success"
 }  
